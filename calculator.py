c854ec6cc05f861a2ad129c38a509b19cdf612f7
Wuthipong Wongwai <145928575+Wutthipong-06@users.noreply.github.com>
2025-12-02 10:41:53 +0700

    Problem Solving-wk2:Success Project in class

---
+++ calculator.py
@@ -0,0 +1,46 @@
+#เมื่อรันเสร็จจะมีไฟล์ calculator เกิดขึ้นมา
+import streamlit as st
+
+def get_number_input(label):
+    return st.number_input(label, value=0.0, step=0.1, format="%.2f")
+
+def get_operator_input(label):
+    valid_operators = ['+', '-', '*', '/', '%']
+    return st.selectbox(label, valid_operators)
+
+def calculate(num1, num2, operator):
+    #copy โค้ดฟังก์ชั่น calculate มาใส่
+    if operator == '+':
+        result = num1 + num2
+    elif operator == '-':
+        result = num1 - num2
+    elif operator == '*':
+        result = num1 * num2
+    elif operator == '/':
+        try:
+            result = num1 / num2
+        except ZeroDivisionError:
+            result = "ไม่สามารถหารด้วย 0 ได้"
+    elif operator == '%':
+        try:
+            result = num1 % num2
+        except ZeroDivisionError:
+            result = "ไม่สามารถหารด้วย 0 ได้"
+    else:
+        result = "การดำเนินการไม่ถูกต้อง"
+    return result
+
+
+
+st.title('My Calculator')
+
+# Get inputs from the user
+num1 = get_number_input("ป้อนตัวเลขแรก:")
+num2 = get_number_input("ป้อนตัวเลขที่สอง:")
+operator = get_operator_input("เลือกตัวดำเนินการ:")
+
+# Perform calculation when the button is clicked
+if st.button('คำนวณ'):
+    result = calculate(num1, num2, operator)
+    if result is not None:
+        st.success(f"ผลลัพธ์: {num1} {operator} {num2} = {result}")
